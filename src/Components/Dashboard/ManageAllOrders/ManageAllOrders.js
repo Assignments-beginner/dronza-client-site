192dd675038fdf3e2ac86e0a08e3679b9253691d
avishekataur <avishekataur@gmail.com>
2021-11-11 13:56:48 +0600

    approve-cancel

--- src/Components/Dashboard/ManageAllOrders/ManageAllOrders.js
+++ src/Components/Dashboard/ManageAllOrders/ManageAllOrders.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Button, Form, Table } from "react-bootstrap";
 
 const ManageAllOrders = () => {
   const [allorders, setAllOrders] = useState([]);
@@ -15,7 +15,7 @@ const ManageAllOrders = () => {
         Manage All <strong style={{ color: "#ea3c23" }}>orders</strong>
       </h3>
       <div>
-        <Table striped bordered hover>
+        <Table bordered hover>
           <thead>
             <tr>
               <th className="text-center">SL</th>
@@ -23,16 +23,27 @@ const ManageAllOrders = () => {
               <th className="text-center">Price</th>
               <th className="text-center">Ordered By</th>
               <th className="text-center">Status</th>
+              <th className="text-center">Approve / Cancel</th>
             </tr>
           </thead>
           <tbody>
             {allorders.map((orders) => (
               <tr key={orders._id} orders={orders}>
-                <td className="text-center">{orders.index}</td>
+                <td className="text-center"></td>
                 <td className="text-center">{orders.singleProductName}</td>
                 <td className="text-center">$ {orders.singleProductPrice}</td>
                 <td className="text-center">{orders.userName}</td>
                 <td className="text-center">{orders.bookedproductStatus}</td>
+                <td className="text-center">
+                  <div className="d-flex">
+                  <Button variant="outline-success w-100 me-2" size="sm">
+                    Approve
+                  </Button>
+                  <Button variant="danger w-100" size="sm">
+                    Cancel
+                  </Button>
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
