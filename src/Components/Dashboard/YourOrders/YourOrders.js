37f053f99adae732aeca9cb407e1eb6cd7b5954e
avishekataur <avishekataur@gmail.com>
2021-11-12 23:44:20 +0600

    netlify-redirect

--- src/Components/Dashboard/YourOrders/YourOrders.js
+++ src/Components/Dashboard/YourOrders/YourOrders.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import Swal from "sweetalert2";
 import useAuth from "../../../Hooks/useAuth";
 
 const YourOrders = () => {
