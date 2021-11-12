28500b7959c78ba5c822320a9766a91d65e80c37
avishekataur <avishekataur@gmail.com>
2021-11-12 11:34:12 +0600

    carousel-slides

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import Reviews from "./Reviews/Reviews";
-import slide1 from "../../Images/download1.svg";
-import slide2 from "../../Images/download2.svg";
-import slide3 from "../../Images/download3.svg";
+import slide1 from "../../Images/slide1.png";
+import slide2 from "../../Images/slide2.png";
+// import slide3 from "../../Images/slide3.png";
 import DisplayProducts from "./DisplayProducts/DisplayProducts";
 
 const Home = () => {
@@ -11,33 +11,21 @@ const Home = () => {
     <div>
       <Carousel variant="dark">
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={slide1}
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={slide1} alt="First slide" />
           <Carousel.Caption>
             <h5>First slide label</h5>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={slide2}
-            alt="Second slide"
-          />
+          <img className="d-block w-100" src={slide2} alt="Second slide" />
           <Carousel.Caption>
             <h5>Second slide label</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={slide3}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src="" alt="Third slide" />
           <Carousel.Caption>
             <h5>Third slide label</h5>
             <p>
