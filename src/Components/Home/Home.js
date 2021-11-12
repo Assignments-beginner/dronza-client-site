48d213d5ce1c846b9fe704d802901afd7c8b5c20
avishekataur <avishekataur@gmail.com>
2021-11-12 13:52:46 +0600

    carousel-done-complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,6 +5,7 @@ import slide1 from "../../Images/slide1.png";
 import slide2 from "../../Images/slide2.png";
 import slide3 from "../../Images/slide3.png";
 import DisplayProducts from "./DisplayProducts/DisplayProducts";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
@@ -14,35 +15,55 @@ const Home = () => {
         <Carousel.Item>
           <img className="d-block w-100" src={slide1} alt="First slide" />
           <Carousel.Caption className="positioting">
-            <h1 className="text-uppercase text-start text-white">
-              <span className="text-danger tiny-shadow">Ultraportable</span> <br /> <span className="tiny-shadow">Drones
-              For Great Videos</span>
+            <h1 className="text-uppercase text-start text-white header">
+              <span style={{ color: "#b91a1a" }} className="tiny-shadow">
+                Ultraportable
+              </span>{" "}
+              <br /> <span className="tiny-shadow">with Great camera</span>
             </h1>
             <p className="text-uppercase text-start text-white">
-              check out our guidelines
+              order your favourite drones now
             </p>
+
+            <Link to="/products">
+              <div className="text-start">
+                <button className="h-btn text-uppercase">check here</button>
+              </div>
+            </Link>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={slide2} alt="Second slide" />
           <Carousel.Caption className="positioting">
             <h1 className="text-uppercase text-start text-white header">
-              <span className="text-danger">phenomenal</span> <br />battery backup
+              <span style={{ color: "#b91a1a" }}>phenomenal</span> <br />
+              battery backup
             </h1>
             <p className="text-uppercase text-start text-white">
-              check out our guidelines
+              order your favourite drones now
             </p>
+            <Link to="/products">
+              <div className="text-start">
+                <button className="h-btn text-uppercase">check here</button>
+              </div>
+            </Link>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={slide3} alt="Third slide" />
           <Carousel.Caption className="positioting">
-            <h1 className="text-uppercase text-start text-white">
-              <span className="text-danger">superfast</span> <br /> <span className="tiny-shadow">with great range</span>
+            <h1 className="text-uppercase text-start text-white header">
+              <span style={{ color: "#b91a1a" }}>superfast</span> <br />{" "}
+              <span className="tiny-shadow">with great range</span>
             </h1>
             <p className="text-uppercase text-start text-white">
-              check out our guidelines
+              order your favourite drones now
             </p>
+            <Link to="/products">
+              <div className="text-start">
+                <button className="h-btn text-uppercase">check here</button>
+              </div>
+            </Link>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
