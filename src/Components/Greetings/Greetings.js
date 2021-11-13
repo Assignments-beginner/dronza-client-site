eb91befe43eedfe64a90e75bb65f7319a85515ff
avishekataur <avishekataur@gmail.com>
2021-11-13 06:24:03 +0600

    editing-greetings

--- src/Components/Greetings/Greetings.js
+++ src/Components/Greetings/Greetings.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Greetings.css";
+import littledrone from '../../Images/registration.png'
 
 const Greetings = () => {
   return (
-    <div className="text-center">
-      <h2 className="p-5 text-uppercase text-dark">
+    <div className="text-center d-flex flex-column justify-content-center align-items-center">
+      <h2 className="mt-5 text-uppercase text-dark">
         Thanks for the registration !
       </h2>
-      <div>
-        <i
-          style={{ color: "#d9534f", fontSize: "150px" }}
-          className="fas fa-heartbeat pb-4 mb-5"
-        ></i>
+      <div className="drone-div">
+        <div className="object border border-dark">
+          <img width="310" height="180" src={littledrone} alt="" />
+        </div>
       </div>
       <div className="pb-5">
-          <h5 className="text-dark">Your account has been created just wait for the reload to autometic sign in. <br /> <br />Let's go to <Link style={{color: "#d9534f", textDecoration:'none'}} to="/home">HOME</Link> page.</h5>
+        <h5 className="text-dark">
+          Your account has been created just wait for the reload to autometic
+          sign in. <br /> <br />
+          Let's go to{" "}
+          <Link style={{ color: "#d9534f", textDecoration: "none" }} to="/home">
+            HOME
+          </Link>{" "}
+          page.
+        </h5>
       </div>
     </div>
   );
