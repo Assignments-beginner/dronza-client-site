ccf968bd095da97fd22c38c841d5da257ff72584
avishekataur <avishekataur@gmail.com>
2021-11-13 06:26:24 +0600

    not-found-done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -5,18 +5,18 @@ const NotFound = () => {
   return (
     <div>
       <h1
-        style={{ fontSize: "140px", color: "#0B114A" }}
+        style={{ fontSize: "140px", color: "#d9534f" }}
         className="notfound mt-5 text-center"
       >
         404
       </h1>
-      <h1 style={{ color: "#3EBBC4" }} className="text-center mb-5 pb-5">
+      <h1 className="text-center text-secondary mb-5 pb-5">
         No Results Found
       </h1>
       <div className="text-center mt-5 pt-5 pb-5">
         <Link className="text-decoration-none" to="/home">
           {" "}
-          <span style={{ color: "#3EBBC4", fontSize: "20px" }}>
+          <span style={{ color: "#d9534f", fontSize: "20px" }}>
             Back To Home
           </span>{" "}
         </Link>
