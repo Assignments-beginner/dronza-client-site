46e58a939e91d6e45483a4377281f77aeae87e36
avishekataur <avishekataur@gmail.com>
2021-12-15 21:25:44 +0600

    contact-res-fixing

--- src/Components/Home/Pages/Contact/Contact.js
+++ src/Components/Home/Pages/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import map from "../../../../Images/location.jpg";
+import "../Contact/Contact.css";
 
 const Contact = () => {
   return (
@@ -10,43 +11,56 @@ const Contact = () => {
       </h2>
       {/* <div style={{ backgroundColor: "#F6F6F6" }} className="main-footer w-100"> */}
       <div className="main-footer w-100">
-        <Container className="d-flex align-items-center justify-content-between py-4">
-          {/* ////////////////////// Contact Us ////////////////////// */}
-          <div className="text-center d-grid me-3">
-            <strong className="text-dark text-uppercase">CONTACT Us</strong>
-            <span className="mb-3">We are open Friday to Wednesday</span>
-            <span>
-              <i className="fas fa-phone text-danger"></i> +1 209-507-5152
-            </span>
-            <span>
-              <i className="fas fa-envelope text-danger"></i>
-              &nbsp;&nbsp;dronza@gmail.com
-            </span>
-          </div>
+        <Container className="py-4">
+          <Row className="d-flex align-items-center justify-content-between">
+            {/* ////////////////////// Contact Us ////////////////////// */}
+            <Col className="text-center d-grid me-3">
+              <strong className="text-dark text-uppercase">
+                connect with us
+              </strong>
+              <span className="mb-3">We are open Friday to Wednesday</span>
+              <span>
+                <i className="fas fa-phone text-danger"></i> +1 209-507-5152
+              </span>
+              <span>
+                <i className="fas fa-envelope text-danger"></i>
+                &nbsp;&nbsp;dronza@gmail.com
+              </span>
+            </Col>
 
-          {/* ////////////////////// Our Locations ////////////////////// */}
-          <div className="text-center mt-2 mb-3">
-            <p className="text-dark text-uppercase">
-              <strong>Our Locations</strong>
-            </p>
-            <div>
-              <img width="355" height="255" src={map} alt="" />
-            </div>
-          </div>
+            {/* ////////////////////// Our Locations ////////////////////// */}
+            <Col className="text-center mt-2 mb-3">
+              <p className="text-dark text-uppercase">
+                <strong>Our Branches</strong>
+              </p>
+              <div>
+                <img
+                  className="res-map"
+                  width="355"
+                  height="255"
+                  src={map}
+                  alt=""
+                />
+              </div>
+            </Col>
 
-          {/*////////////////////// ADDRESS //////////////////////*/}
-          <div className="text-center d-grid">
-            <strong className="text-dark text-uppercase">address</strong>
-            <span>2859 Wilkinson Street</span>
-            <span>California St, Stockton, United States</span>
-
-            <div className="mt-4">
-              <i className="fas fa-truck text-danger"></i>&nbsp;&nbsp;
-              <strong className="text-dark text-uppercase">Delivery</strong>
-            </div>
-            <span>Under City 1Week</span>
-            <span>Under Province 2Weeks</span>
-          </div>
+            {/*////////////////////// ADDRESS //////////////////////*/}
+            <Col className="text-center d-grid">
+              <div>
+                <strong className="text-dark text-uppercase">address</strong>{" "}
+                <i class="fas fa-map-marker-alt text-danger"></i>
+              </div>
+              <span>2859 Wilkinson Street</span>
+              <span>California St, Stockton, United States</span>
+              <div className="mt-4">
+                <strong className="text-dark text-uppercase">Delivery</strong>
+                &nbsp;&nbsp;
+                <i className="fas fa-truck text-danger"></i>
+              </div>
+              <span>Under City 1Week</span>
+              <span>Under Province 2Weeks</span>
+            </Col>
+          </Row>
         </Container>
       </div>
     </div>
