b804e5b07f2f5d31ccb1749f9df0fe6d8d1e8907
avishekataur <avishekataur@gmail.com>
2022-01-07 21:40:32 +0600

    payment-intent

--- src/Components/Dashboard/Payment/CheckoutForm.js
+++ src/Components/Dashboard/Payment/CheckoutForm.js
@@ -2,13 +2,33 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { Button, Container } from "react-bootstrap";
+import useAuth from "../../../Hooks/useAuth";
 
 const CheckoutForm = ({ payment }) => {
-  const { paymentPrice } = payment;
+  const { paymentPrice, _id, userName } = payment;
   const stripe = useStripe();
   const elements = useElements();
+  const { user } = useAuth();
 
   const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://morning-badlands-81993.herokuapp.com/create-payment-intent",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ paymentPrice }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [paymentPrice]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,6 +41,7 @@ const CheckoutForm = ({ payment }) => {
       return;
     }
 
+    //Payment Intent
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: "card",
       card,
@@ -31,6 +52,43 @@ const CheckoutForm = ({ payment }) => {
     } else {
       setCardError("");
       console.log("[PaymentMethod]", paymentMethod);
+    }
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: userName,
+            email: user.email,
+          },
+        },
+      });
+
+    if (intentError) {
+      setCardError(intentError.message);
+      setSuccess("");
+    } else {
+      setCardError("");
+      setSuccess("Your payment processed successfully.");
+      console.log(paymentIntent);
+      setProcessing(false);
+      // Save To Database
+      const payment = {
+        amount: paymentIntent.amount,
+        created: paymentIntent.created,
+        last4: paymentMethod.card.last4,
+        transaction: paymentIntent.client_secret.slice("_secret")[0],
+      };
+      const url = `https://morning-badlands-81993.herokuapp.com/allorders/${_id}`;
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(payment),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log(data));
     }
   };
   return (
@@ -60,13 +118,11 @@ const CheckoutForm = ({ payment }) => {
             type="submit"
             disabled={!stripe}
           >
-            Pay ${paymentPrice}
+            Pay&nbsp; ${paymentPrice}
           </Button>
         </Container>
       </form>
-      {
-          cardError && <p className="text-center text-danger">{cardError}</p>
-      }
+      {cardError && <p className="text-center text-danger">{cardError}</p>}
     </div>
   );
 };
