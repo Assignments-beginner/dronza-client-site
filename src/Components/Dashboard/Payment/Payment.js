978359fef0042a4ddbfa358bc8d6d295d8c9161d
avishekataur <avishekataur@gmail.com>
2022-01-07 11:51:42 +0600

    payment-fetch-done

--- src/Components/Dashboard/Payment/Payment.js
+++ src/Components/Dashboard/Payment/Payment.js
@@ -1,37 +1,50 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./Payment.css";
 import { useParams } from "react-router";
 import { Button, Col, Container, Row } from "react-bootstrap";
+
 const Payment = () => {
   const { paymentId } = useParams();
+  const [payment, setPayment] = useState({});
+
+  useEffect(() => {
+    fetch(`https://morning-badlands-81993.herokuapp.com/allorders/${paymentId}`)
+      .then((res) => res.json())
+      .then((data) => setPayment(data));
+  }, [paymentId]);
   return (
     <>
-      <h3 className="dashboard-sectionTitle text-center text-dark mb-5 text-uppercase">
+      <h3 className="dashboard-sectionTitle text-center text-dark mb-3 text-uppercase">
         Payment
       </h3>
-      <Container className="payment-frame mb-5 p-5">
+      <Container className="payment-frame mb-5 pt-3">
         <Row>
           {/* Name  */}
           <Col>
             {" "}
-            <span className="label text-center">Name</span>{" "}
+            <span className="label text-center">Name</span>
+            {payment.userName}
           </Col>
           {/* Product  */}
           <Col>
             {" "}
             <span className="label text-center">Product</span>{" "}
+            {payment.singleProductName}
           </Col>
         </Row>
         <Row>
           {/* Model  */}
           <Col>
             {" "}
-            <span className="label text-center">Model</span>{" "}
+            <span className="label text-center">Model</span>
+            {payment.singleProductModel}
           </Col>
           {/* Price  */}
           <Col>
             {" "}
-            <span className="label text-center">Price</span>{" "}
+            <span className="label text-center">Price</span>
+            {payment.singleProductPrice}
           </Col>
         </Row>
       </Container>
