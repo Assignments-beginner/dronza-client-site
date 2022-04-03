5d8c811683e6486ed25c8e8c51fd326a5dfc6054
avishekataur <avishekataur@gmail.com>
2022-04-03 15:57:01 +0600

    fixing_signIn_page

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -72,65 +72,71 @@ const LogIn = () => {
   };
 
   return (
-    <Row>
-      <Col lg={4} xl={4} sm={12} className="mt-4 mx-auto px-5">
-      <p className="text-uppercase fs-2">
-        Please <span className="text-danger">Sign In</span>{" "}
-      </p>
-      <Form>
-        {/* Email Field */}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            onClick={removeError}
-            onBlur={emailBlurHandler}
-            type="email"
-            placeholder="Enter Email"
-          />
-        </Form.Group>
-        {/* Password Field */}
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            onClick={removeError}
-            onBlur={passwordBlurHandler}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        {/* Not having an account */}
-        <div className="mb-3">
-          <span>Don't have an account?</span>
-          &nbsp; &nbsp;
-          <Link
-            className="text-danger"
-            onClick={removeError}
-            to="/registration"
-          >
-            Create One
-          </Link>
-        </div>
-        {/* Display Error */}
-        <p className="text-danger">{error}</p>
-        {/* Register Button */}
-        <Button onClick={signInHandler} variant="danger" type="submit">
-          Sign In
-        </Button>
-      </Form>
-
-      <div className="mt-3 mb-5 d-grid justify-items-center">
-        <p className="text-secondary mb-3 text-center">Or you can also</p>
-        {/* Button */}
-        <button onClick={googleLoginHandler} className="mx-auto signin-btn">
-          <div className="d-flex align-items-center justify-content-between">
-            <img height="32" width="32" src={google} alt="" />{" "}
-            <span>Continue With Google</span>{" "}
-            <span className="text-white">G</span>
-          </div>
-        </button>
+    <div className="signin-banner d-flex align-items-start justify-content-between">
+      <div>
+        <h1 className="border border-danger text-white">NOTHING</h1>
       </div>
-    </Col>
-    </Row>
+      <div lg={4} xl={4} sm={12} className="border border-danger px-5">
+        <h1 className="text-uppercase">
+          <span style={{ color: "#B91A1A" }}>Please</span>&nbsp;
+          <span className="text-white">Sign In</span>{" "}
+        </h1>
+        <Form>
+          {/* Email Field */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label className="text-white fs-5">Email</Form.Label>
+            <Form.Control
+              className="border border-2 border-danger"
+              style={{ backgroundColor: "rgba(0,0,0,0)" }}
+              onClick={removeError}
+              onBlur={emailBlurHandler}
+              type="email"
+              placeholder="Enter Email"
+            />
+          </Form.Group>
+          {/* Password Field */}
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label className="text-white fs-5">Password</Form.Label>
+            <Form.Control
+              className="border border-2 border-danger"
+              style={{ backgroundColor: "rgba(0,0,0,0)" }}
+              onClick={removeError}
+              onBlur={passwordBlurHandler}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          {/* Not having an account */}
+          <div className="mb-3">
+            <span className="text-white">Don't have an account?</span>
+            &nbsp; &nbsp;
+            <Link
+              className="text-danger"
+              onClick={removeError}
+              to="/registration"
+            >
+              Create One
+            </Link>
+          </div>
+          {/* Display Error */}
+          <p className="text-danger">{error}</p>
+          {/* Register Button */}
+          <Button onClick={signInHandler} variant="danger" type="submit">
+            Sign In
+          </Button>
+        </Form>
+        <div className="mt-3 mb-5 d-grid justify-items-center">
+          {/* Button */}
+          <button onClick={googleLoginHandler} className="mx-auto signin-btn">
+            <div className="d-flex align-items-center justify-content-between">
+              <img height="32" width="32" src={google} alt="" />{" "}
+              <span>Continue With Google</span>{" "}
+              <span style={{ color: "#B91A1A" }}>G</span>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
