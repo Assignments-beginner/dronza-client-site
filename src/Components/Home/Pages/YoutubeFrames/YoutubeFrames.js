4f8c4b9f7abbf07527d7795d50fc333c81c0a38b
avishekataur <avishekataur@gmail.com>
2022-04-06 02:48:56 +0600

    gif_frames

--- src/Components/Home/Pages/YoutubeFrames/YoutubeFrames.js
+++ src/Components/Home/Pages/YoutubeFrames/YoutubeFrames.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import FrameCard from "../YoutubeFrames/FrameCard/FrameCard";
+import accDrone1 from "../../../../Images/accDrone1.jpg";
+import accDrone2 from "../../../../Images/accDrone2.jpg";
+import accDrone3 from "../../../../Images/accDrone3.jpg";
 
 const YoutubeFrames = () => {
   const [videos, setvideos] = useState([]);
@@ -14,49 +18,85 @@ const YoutubeFrames = () => {
   }, []);
 
   return (
-    <div className="pt-5">
+    <Container className="pt-5">
       <h2 className="text-center text-dark text-uppercase mb-3">
         Check Out <span className="text-danger">accessories</span> Update
       </h2>
-      <p className="text-uppercase text-center">under construction</p>
-      <div className="d-flex justify-content-center border border-danger p-5">
-        <div className="d-flex align-items-center">
-          <div className="p-4">
-            <Button
-              onClick={() => setActive("firstCard")}
-              variant="outline-danger m-2"
-            >
-              Click1
-            </Button>
-            <br />
-            <Button
-              onClick={() => setActive("secondCard")}
-              variant="outline-danger m-2"
-            >
-              Click2
-            </Button>
-            <br />
-            <Button
-              onClick={() => setActive("thirdCard")}
-              variant="outline-danger m-2"
-            >
-              Click3
-            </Button>
-          </div>
-          <div className="p-4">
-            {active === "firstCard" && (
-              <FrameCard videos={videos} cardIndex={0} />
-            )}
-            {active === "secondCard" && (
-              <FrameCard videos={videos} cardIndex={1} />
-            )}
-            {active === "thirdCard" && (
-              <FrameCard videos={videos} cardIndex={2} />
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
+      <Row className="border border-danger">
+        <Col className="m-4 border border-danger">
+          <Link
+            to=""
+            className="text-decoration-none text-dark"
+            onClick={() => setActive("firstCard")}
+          >
+            <Card className="flex-row align-items-center mb-4">
+              <Card.Img
+                style={{ width: "175px", height: "135px" }}
+                src={accDrone1}
+              />
+              <Card.Body>
+                <Card.Text>12/04/2021</Card.Text>
+                <Card.Title>Smooth Flexible Rotation</Card.Title>
+                <Card.Text>
+                  <i class="fas fa-camera text-danger"></i> &nbsp;Motion Camera
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+          <Link
+            to=""
+            className="text-decoration-none text-dark"
+            onClick={() => setActive("secondCard")}
+          >
+            <Card className="flex-row align-items-center mb-4">
+              <Card.Img
+                style={{ width: "175px", height: "135px" }}
+                src={accDrone2}
+              />
+              <Card.Body>
+                <Card.Text>12/02/2022</Card.Text>
+                <Card.Title>Full Quality Performance</Card.Title>
+                <Card.Text>
+                  <i className="fas fa-cog text-danger"></i> &nbsp;Best
+                  Accessories
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+          <Link
+            to=""
+            className="text-decoration-none text-dark"
+            onClick={() => setActive("thirdCard")}
+          >
+            <Card className="flex-row align-items-center">
+              <Card.Img
+                style={{ width: "175px", height: "135px" }}
+                src={accDrone3}
+              />
+              <Card.Body>
+                <Card.Text>02/04/2022</Card.Text>
+                <Card.Title>On Long Distance Range</Card.Title>
+                <Card.Text>
+                  <i class="fas fa-map-marker-alt text-danger"></i>{" "}
+                  &nbsp;Location Track
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+        <Col className="m-4 border border-danger d-flex align-items-center">
+          {active === "firstCard" && (
+            <FrameCard videos={videos} cardIndex={0} />
+          )}
+          {active === "secondCard" && (
+            <FrameCard videos={videos} cardIndex={1} />
+          )}
+          {active === "thirdCard" && (
+            <FrameCard videos={videos} cardIndex={2} />
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/Components/Home/Pages/YoutubeFrames/frameCard/frameCard.js
+++ src/Components/Home/Pages/YoutubeFrames/frameCard/frameCard.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import java from "../../../../../Images/frame1.gif";
 
 const FrameCard = ({ videos, cardIndex }) => {
   return (
     <div>
       {videos[cardIndex]?.map((video) => (
-        <div className="border border-danger">
-          <p>{video.title}</p>
-          <p>{video.name}</p>
-        </div>
+        <Card>
+          <Card.Img style={{ width: "100%" }} src={java} />
+          <Card.Title className="text-center uppercase py-4">
+            Feel Free To Visit Our Channel
+          </Card.Title>
+        </Card>
       ))}
     </div>
   );
